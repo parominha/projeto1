0893417eab9a0b407cac7f094c9a237f8d50e431
paloma.asantos17 <paloma.asantos17@OSA0625240W10-1>
2022-08-01 21:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroChamada = 0;
+            string nomeAluno = "Christian Perin";
+
+            numeroChamada = 1;
+
+            Console.WriteLine(numeroChamada);
+            Console.WriteLine(nomeAluno);
+
+            Console.WriteLine(args[0].ToString());
+            Console.WriteLine(args[1].ToString());
+            Console.WriteLine(args[2].ToString());
+            Console.WriteLine(args[3].ToString());
+
+            Console.WriteLine("Hello World!");
+        }
+    } 
+}
